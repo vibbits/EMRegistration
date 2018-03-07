dd7d9ab618c783d58a385583d5ef8f55ff235193
Frank Vernaillen <frank.vernaillen@ugent.be>
2018-03-07 16:05:05 +0100

    Initial version of main file

---
+++ src/main/java/be/vib/imagej/RegistrationIJ2.java
@@ -0,0 +1,26 @@
+package be.vib.imagej;
+
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, menuPath = "Plugins>EM Registration>Register")
+public class RegistrationIJ2 implements Command
+{
+	@Parameter
+	private LogService log;
+	
+	@Override
+	public void run() 
+	{
+		log.info("VIB EM Registration plugin");
+		
+		Wizard wizard = RegistrationWizardSingleton.getInstance();
+		wizard.setVisible(true);
+		
+		// After displaying the registration wizard the ImageJ plugin run() method finishes immediately,
+		// but the wizard is still visible and active.
+	}
+}
+	
